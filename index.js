b822bde7558741f71dfbfb4ad9950b2f587a291f
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-05-09 15:44:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const pTag = document.getElementById('p-tag');
+pTag.innerHTML = 'New Sentence';
+pTag.addEventListener('click', function () {
+  alert('I have been clicked');
+});
+// window.alert('Hello')
+// console.log(pTag);
---
+++ style.js
@@ -0,0 +1,11 @@
+let name = "lizzy";
+console.log(name);
+
+let fruits = ["Orange", "Apple", "Cherrie"];
+console.log(fruits);
+
+function greeting() {
+    return "Hello";
+}
+
+console.log(greeting())
